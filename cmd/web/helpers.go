d893f5353c8a48764dce94147914a005bf85e414
qiuzhanghua <qiuzhanghua@icloud.com>
2023-07-21 06:26:25 +0800

    CSRF

--- cmd/web/helpers.go
+++ cmd/web/helpers.go
@@ -5,6 +5,7 @@ import (
 	"errors"
 	"fmt"
 	"github.com/go-playground/form/v4"
+	"github.com/justinas/nosurf"
 	"net/http"
 	"runtime/debug"
 	"time"
@@ -52,6 +53,7 @@ func (app *application) newTemplateData(r *http.Request) *templateData {
 		CurrentYear:     time.Now().Year(),
 		Flash:           app.sessionManager.PopString(r.Context(), "flash"),
 		IsAuthenticated: app.IsAuthenticated(r),
+		CSRFToken:       nosurf.Token(r),
 	}
 }
 
--- go.mod
+++ go.mod
@@ -13,4 +13,7 @@ require (
 	golang.org/x/crypto v0.11.0
 )
 
-require gopkg.in/go-playground/assert.v1 v1.2.1 // indirect
+require (
+	github.com/justinas/nosurf v1.1.1 // indirect
+	gopkg.in/go-playground/assert.v1 v1.2.1 // indirect
+)
