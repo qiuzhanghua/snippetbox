b5f7c7d1df4808e13e72db49376fa596ec21b94c
qiuzhanghua <qiuzhanghua@icloud.com>
2023-07-20 15:38:06 +0800

    User Manager

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -20,6 +20,7 @@ type application struct {
 	errorLog       *log.Logger
 	infoLog        *log.Logger
 	snippets       *models.SnippetModel
+	users          *models.UserModel
 	templateCache  map[string]*template.Template
 	formDecoder    *form.Decoder
 	sessionManager *scs.SessionManager
@@ -50,6 +51,7 @@ func main() {
 		errorLog:       errorLog,
 		infoLog:        infoLog,
 		snippets:       &models.SnippetModel{DB: db},
+		users:          &models.UserModel{DB: db},
 		templateCache:  templateCache,
 		formDecoder:    form.NewDecoder(),
 		sessionManager: sessionManager,
