0925731ff94fa5575cbd25347ad7082556a7dd76
qiuzhanghua <qiuzhanghua@icloud.com>
2023-07-20 19:32:23 +0800

    User login

--- internal/validator/validator.go
+++ internal/validator/validator.go
@@ -7,11 +7,16 @@ import (
 )
 
 type Validator struct {
-	FieldErrors map[string]string
+	NonFieldErrors []string
+	FieldErrors    map[string]string
 }
 
 func (v *Validator) Valid() bool {
-	return len(v.FieldErrors) == 0
+	return len(v.NonFieldErrors) == 0 && len(v.FieldErrors) == 0
+}
+
+func (v Validator) AddNonFieldError(message string) {
+	v.NonFieldErrors = append(v.NonFieldErrors, message)
 }
 
 func (v *Validator) AddFieldError(field string, message string) {
