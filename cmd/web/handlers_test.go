47cd66c12a23ac9d5f634ebecc50a7ef4e6d0c5e
qiuzhanghua <qiuzhanghua@icloud.com>
2023-07-21 08:16:21 +0800

    test end to end

--- cmd/web/handlers_test.go
+++ cmd/web/handlers_test.go
@@ -3,6 +3,7 @@ package main
 import (
 	"bytes"
 	"io"
+	"log"
 	"net/http"
 	"net/http/httptest"
 	"taiji.dev/snippetbox/internal/assert"
@@ -14,6 +15,24 @@ func TestPing(t *testing.T) {
 	r := httptest.NewRequest("GET", "/ping", nil)
 	ping(rr, r)
 	rs := rr.Result()
+	assert.Equal(t, rs.StatusCode, http.StatusOK)
+	defer rs.Body.Close()
+	body, err := io.ReadAll(rs.Body)
+	assert.Equal(t, err, nil)
+	bytes.TrimSpace(body)
+	assert.Equal(t, string(body), "OK")
+}
+
+func TestPingE2E(t *testing.T) {
+	app := &application{
+		errorLog: log.New(io.Discard, "", 0),
+		infoLog:  log.New(io.Discard, "", 0),
+	}
+	ts := httptest.NewServer(app.routes())
+	defer ts.Close()
+
+	rs, err := ts.Client().Get(ts.URL + "/ping")
+	assert.Equal(t, err, nil)
 	assert.Equal(t, rs.StatusCode, http.StatusOK)
 	defer rs.Body.Close()
 	body, err := io.ReadAll(rs.Body)
